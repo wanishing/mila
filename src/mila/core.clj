6dc1a2b4c22eeac0075c2308b65e846c0316443c
talwanish <tal.vanish@gmail.com>
2024-06-27 16:36:35 +0300

    u

---
+++ src/mila/core.clj
@@ -0,0 +1,22 @@
+(ns mila.core
+  "FIXME: my new org.corfield.new/scratch project.")
+
+
+(defn exec
+  "Invoke me with clojure -X mila.mila/exec"
+  [opts]
+  (println "exec with" opts))
+
+
+(defn -main
+  "Invoke me with clojure -M -m mila.mila"
+  [& args]
+  (println "-main with" args))
+
+
+(comment
+
+  (def chrb-setup "/Users/talwanish/dev/chargeback-trainer/setup.py")
+  
+  (def chrb-str )
+)
